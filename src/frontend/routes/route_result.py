cf42790aae22ec89432f824a9fbcf7dc4579aab6
DaMuBo <daniel910527@googlemail.com>
2023-12-28 21:48:03 +0100

    added some route info

--- src/frontend/routes/route_result.py
+++ src/frontend/routes/route_result.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 
+import threading
+
 from flask import render_template, request, redirect, url_for, Blueprint
 from PIL import Image
 from src.functions.arrange_images import arrange_images
 from src.functions.get_layout import get_layout_random_numeric
+# from src.functions.printout import print_image_cups # only for test on linux
 
 
 ROOT = Path(__file__).resolve().parent.parent.parent.parent
@@ -16,10 +19,11 @@ result_bp = Blueprint('result_bp', __name__)
 
 @result_bp.route('/result/<int:num_images>', methods=['GET', 'POST'])
 def result(num_images):
-    if request.method == 'POST':
-        return redirect(url_for("qr_code_bp.qr_code"))
-
     result_image_path = ROOT / "samples" / "test_img.jpg"
+    if request.method == 'POST':
+        image = Image.open(result_image_path)
+        # print_image_cups(image, os.getenv("PRINTER_NAME"))
+        return redirect(url_for("qr_code_bp.qr_code"))
 
     layout_img = get_layout_random_numeric(ROOT / "samples" / "layouts", num_images)
     layout_text = os.getenv("LAYOUT_TEXT", None)
@@ -30,7 +34,6 @@ def result(num_images):
                                   layout_image=layout_img,
                                   layout_text=layout_text
                                   )
-    result_image = result_image.resize((800, 600))
     save_path = static / "test_img_2.jpg"
     result_image.save(str(save_path))
 

25533ecaa05ce03ac3b8f639e5266bcc51f15da8
DaMuBo <daniel910527@googlemail.com>
2024-02-27 23:06:25 +0100

    fixed an error

--- src/functions/make_qr_code.py
+++ src/functions/make_qr_code.py
@@ -8,5 +8,5 @@ def make_qr_code(link: Path) -> str:
     :param link:
     :return: string base64 encoded png of qr code
     """
-    code = segno.make_qr(str(link)).png_data_uri(scale=10)
+    code = segno.make_qr(str(link)).png_data_uri(scale=5)
     return code
